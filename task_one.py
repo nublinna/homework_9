a27daa6fb36526d0549bf001571410dd8c1d1043
nublinna <nikachechko519@gmail.com>
2025-11-07 22:29:37 +0300

    first

---
+++ task_one.py
@@ -0,0 +1,165 @@
+# Класс «Товар» содержит следующие закрытые поля:
+# название товара
+# название магазина, в котором продаётся товар
+# стоимость товара в рублях
+#
+# Класс «Склад» содержит закрытый массив товаров.
+# Обеспечить следующие возможности:
+# вывод информации о товаре со склада по индексу
+# вывод информации о товаре со склада по имени товара
+# сортировка товаров по названию, по магазину и по цене
+# перегруженная операция сложения товаров по цене
+
+
+class Product:
+    def __init__(self, name, shop_name, price):
+        self.__name = name
+        self.__shop_name = shop_name
+        self.__price = price
+
+    def get_name(self):
+        return self.__name
+
+    def get_shop_name(self):
+        return self.__shop_name
+
+    def get_price(self):
+        return self.__price
+
+    def __add__(self, other):
+        if isinstance(other, Product):
+            return self.__price + other.__price
+        else:
+            raise ValueError("")
+
+
+class Storage:
+    def __init__(self):
+        self.__storage = []
+
+    def add_product(self, product):
+        self.__storage.append(product)
+
+    def get_product_by_index(self, index):
+        if index >= 0 and index < len(self.__storage):
+            return self.__storage[index]
+        else:
+            return None
+
+
+    def get_product_by_name(self, name):
+        found_product = []
+        for product in self.__storage:
+            product_name = product.get_name().lower()
+            if product_name == name.lower():
+                found_product.append(product)
+        if found_product:
+            return found_product
+        return None
+
+    def sort_by_name(self):
+        self.__storage.sort(key=lambda product: product.get_name())
+
+    def sort_by_shop_name(self):
+        self.__storage.sort(key=lambda product: product.get_shop_name())
+
+    def sort_by_price(self):
+        self.__storage.sort(key=lambda product: product.get_price())
+
+    def sort_by_reverse_price(self):
+        self.__storage.sort(key=lambda product: product.get_price(), reverse=True)
+
+    def display_products(self):
+        if not self.__storage:
+            print("Склад пуст")
+            return
+
+        print("\nТовары на складе:")
+        for i, product in enumerate(self.__storage, 1):
+            print(f"\nИндекс товара: {i}"
+                  f"\nНазвание: {product.get_name()}"
+                  f"\nМагазин: {product.get_shop_name()}"
+                  f"\nЦена: {product.get_price()} руб.")
+
+
+product_one = Product("Ноутбук", "5 Элемент", 2500)
+product_two = Product("Кофемашина", "DNS", 1000)
+product_three = Product("Телефон", "DNS", 1000)
+product_four = Product("Стиральная машина", "Samsung", 3000)
+
+storage = Storage()
+storage.add_product(product_one)
+storage.add_product(product_two)
+storage.add_product(product_three)
+storage.add_product(product_four)
+
+storage.display_products()
+
+while True:
+    user_input = input("\nВыберите режим программы:"
+                       "\n1 -- Поиск товара по индексу"
+                       "\n2 -- Поиск товара по названию"
+                       "\n3 -- Сортировка товаров по названию товара"
+                       "\n4 -- Сортировка товаров по названию магазина"
+                       "\n5 -- Сортировка товаров по цене (возрастанию/убыванию)"
+                       "\n6 -- Сложение товаров по цене"
+                       "\n7 -- Выход из программы: ")
+
+
+    if user_input == "1":
+        index = input("Введите индекс товара: ")
+        while True:
+            if index.isdigit():
+                index = int(index)
+                break
+            else:
+                print("Неверный ввод! Введите целое число")
+
+        found_product = storage.get_product_by_index(index)
+        if found_product:
+            print(f"Товар найден:"
+                  f"\nНазвание: {found_product.get_name()}"
+                  f"\nМагазин: {found_product.get_shop_name()}"
+                  f"\nЦена: {found_product.get_price()} руб.")
+        else:
+            print("Товар не найден")
+
+    if user_input == "2":
+        foud_by_name = input("Введите название товара: ")
+        found_product_by_name = storage.get_product_by_name(foud_by_name)
+        if found_product_by_name:
+            for product in found_product_by_name:
+                print(f"Товар найден:"
+                    f"\nНазвание: {product.get_name()}"
+                    f"\nМагазин: {product.get_shop_name()}"
+                    f"\nЦена: {product.get_price()} руб.")
+        else:
+            print("Товар не найден")
+
+    if user_input == "3":
+        print("\nСортировка по названию товара")
+        storage.sort_by_name()
+        storage.display_products()
+
+    if user_input == "4":
+        print("\nСортировка по названию магазина")
+        storage.sort_by_shop_name()
+        storage.display_products()
+
+    if user_input == "5":
+        print("\nСортировка по цене (по возрастанию)")
+        storage.sort_by_price()
+        storage.display_products()
+
+        print("\nСортировка по цене (по убыванию)")
+        storage.sort_by_reverse_price()
+        storage.display_products()
+
+    if user_input == "6":
+        result_one = product_one + product_two
+        result_two = product_three + product_four
+        result = result_one + result_two
+        print(f"Сумма цен всех товаров {result} руб.")
+
+    if user_input == "7":
+        break
---
+++ task_two.py
@@ -0,0 +1,105 @@
+# Экземпляр класса инициализируется двумя целыми числами, первое относится к пчеле, второе – к слону.
+# Класс реализует следующие методы:
+# fly() – возвращает True, если часть пчелы не меньше части слона, иначе – False
+# trumpet() – если часть слона не меньше части пчелы, возвращает строку “tu-tu-doo-doo”, иначе – “wzzzz”
+# eat(meal, value) – может принимать в meal только ”nectar” или “grass”.
+# Если съедает нектар, то value вычитается из части слона, пчеле добавляется.
+# Иначе – наоборот. Не может увеличиваться больше 100 и уменьшаться меньше 0.
+
+
+class BeeElefant:
+    def __init__(self, bee_part, elefant_part):
+        self.bee_part = bee_part
+        self.elefant_part = elefant_part
+
+    def fly(self):
+        if self.bee_part >= self.elefant_part:
+            return True
+        else:
+            return False
+
+    def trumpet(self):
+        if self.elefant_part >= self.bee_part:
+            return "tu-tu-doo-doo!"
+        else:
+            return "wzzzz"
+
+    def eat(self, meal, value):
+        if meal == "nectar":
+            self.elefant_part -= value
+            self.bee_part += value
+        elif meal == "grass":
+            self.elefant_part += value
+            self.bee_part -= value
+
+        if self.elefant_part > 100:
+            self.elefant_part = 100
+        elif self.elefant_part < 0:
+            self.elefant_part = 0
+        if self.bee_part > 100:
+            self.bee_part = 100
+        elif self.bee_part < 0:
+            self.bee_part = 0
+
+    def __str__(self):
+        return f"BeeElefant(bee={self.bee_part}, elephant={self.elefant_part})"
+
+while True:
+    bee_part = input("Введите пчелиную часть: ")
+    elefant_part = input("Введите слоновью часть: ")
+    if bee_part.isdigit() and elefant_part.isdigit():
+        bee_part = int(bee_part)
+        elefant_part = int(elefant_part)
+        break
+    else:
+        print("Введите целые числа!")
+bee_elefant = BeeElefant(bee_part, elefant_part)
+print(f"\nПчёлоСлон готов!"
+      f"\nПчелиная часть: {bee_part}"
+      f"\nСлоновья часть: {elefant_part}")
+
+print("\nМожет ли ваш ПчёлоСлон летать?")
+if bee_elefant.fly() is True:
+    print("Да, может!")
+else:
+    print("Нет, не может!")
+
+print("\nКакие звуки издает ваш ПчёлоСлон?")
+print(bee_elefant.trumpet())
+
+eat_input = input("\nЧем вы хотите накормить ПчёлоСлона?"
+                  "\n1 -- нектар"
+                  "\n2 -- трава: ")
+
+if eat_input == "1":
+    while True:
+        value = input("Введите количество съеденного: ")
+        if value.isdigit():
+            value = int(value)
+            break
+        else:
+            print("Введите целое число!")
+    bee_elefant.eat("nectar", value)
+
+if eat_input == "2":
+    while True:
+        value = input("Введите количество съеденного: ")
+        if value.isdigit():
+            value = int(value)
+            break
+        else:
+            print("Введите целое число!")
+    bee_elefant.eat("grass", value)
+
+print(f"\nПчёлоСлон после легкого перекуса"
+      f"\nПчелиная часть: {bee_elefant.bee_part}"
+      f"\nСлоновья часть: {bee_elefant.elefant_part}")
+
+print("\nМожет ли теперь ваш ПчёлоСлон летать?")
+if bee_elefant.fly() is True:
+    print("Да, может!")
+else:
+    print("Нет, не может!")
+
+print("\nКакие звуки теперь издает ваш ПчёлоСлон?")
+print(bee_elefant.trumpet())
